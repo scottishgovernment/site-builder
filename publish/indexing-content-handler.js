4d7dfc0261672c61216ef4fc439b154bc5c44c21
David Sinclair <David.Sinclair@gov.scot>
2015-10-19 16:30:37 +0100

    added topicNamesProperty to allow filtering on topics landing page

--- publish/indexing-content-handler.js
+++ publish/indexing-content-handler.js
@@ -29,6 +29,12 @@ module.exports = function (searchURL) {
             contentItem.url = item.url;
             contentItem._embedded.format.name = contentItem._embedded.format.name.toLowerCase();
 
+            // Hack for topics ...
+            contentItem.topicNames = [];
+            contentItem._embedded.topics.forEach(function (t) {
+                contentItem.topicNames.push(t.name);
+            });
+
             restler.putJson(searchURL, contentItem)
                 .on('complete', function() {
                      //Should log the failure to index
