2d85998549184dc78b0e110d639f9a97589ca958
Tim Squires <tim.squires@scotland.gsi.gov.uk>
2015-11-04 15:53:59 +0000

    Added handler for static doctor landing pages

--- publish/content-source.js
+++ publish/content-source.js
@@ -5,10 +5,23 @@ module.exports = function (config, contentFormatter, contentHandler) {
 
     var restler = require('restler');
     var async = require('async');
+    var fs = require('fs');
 
     // return a url for this item (if empty then will return the url for all items)
     function url(id) {
         return config.buildapi.endpoint + 'items/' + id + '?visibility=siteBuild';
+    }
+
+    function processJson(data, callback){
+       try {
+            var contentItem = contentFormatter.format(JSON.parse(data));
+            contentHandler.handleContentItem(contentItem,
+                                            function() {
+                                                callback(null, contentItem);
+                                            });
+        } catch (error) {
+           callback(error);
+        }
     }
 
     // fetch the item with this id
@@ -20,39 +33,55 @@ module.exports = function (config, contentFormatter, contentHandler) {
                     if (data instanceof Error || response.statusCode !== 200) {
                         callback(data);
                     } else {
-                        try {
-                            var contentItem = contentFormatter.format(JSON.parse(data));
-                            contentHandler.handleContentItem(contentItem,
-                                function () {
-                                    callback(null, contentItem);
-                                });
-                        } catch (error) {
-                            callback(error);
-                        }
+                        processJson(data, callback);
                     }
                 });
     }
 
     // fetch all published IDs then fetch each item in turn
-    function fetchIDs(callback) {
+    function processContentItems(callback) {
         var publishedIDsURL = url('');
         restler.get(publishedIDsURL)
             .on('complete', function(data, response) {
                 if (data instanceof Error || response.statusCode !== 200) {
-                    contentHandler.end(data, callback);
+                    callback(data);
                 } else {
                     try {
                         var ids = JSON.parse(data);
 
-                        async.each(ids, fetchItem,
-                            function(err) {
-                                contentHandler.end(err, callback);
-                            });
+                        async.each(ids, fetchItem, callback );
                     } catch (error) {
-                        contentHandler.end(error, callback);
+                        callback(error);
                     }
                 }
             });
+    }
+
+    function processStaticFile(filename, callback) {
+        var json = "resources/doctor/"+filename+"/index.json";
+        console.log("processing doctor file "+json);
+        fs.readFile( json, 'utf8',
+                    function(err, data) {
+                                if (err){
+                                    console.log(err);
+                                    callback(err);
+                                } else
+                                {
+                                    processJson(data, callback);
+                                }
+                            });
+
+    }
+
+    function processStaticFiles(callback) {
+        console.log("Starting to doctor files....");
+        fs.readdir('resources/doctor/', function(err, files){
+            async.each(files,  processStaticFile,
+                            function(err) {
+                                console.log(err);
+                                callback(err);
+                            });
+        });
     }
 
     return {
@@ -62,12 +91,14 @@ module.exports = function (config, contentFormatter, contentHandler) {
         },
 
         getContent: function (callback) {
-             // tell the content Handler we are starting so that it can perform
-            contentHandler.start(
-                function () {
-                    // start by fetching the published IDs
-                    fetchIDs(callback);
-                });
+                    async.series([
+                            contentHandler.start,
+                            processContentItems, 
+                            processStaticFiles
+                        ], 
+                        function(err){
+                            contentHandler.end(err, callback);
+                        });
         }
     };
 };
