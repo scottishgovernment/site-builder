10c4b9b4355203a895e450d5287beeb9cfb5984c
Evren Ozcan <evren.ozcan@scotland.gsi.gov.uk>
2016-06-06 16:19:42 +0100

    MGS-1240 Added TODO section for site specific content

--- preview/server.js
+++ preview/server.js
@@ -9,9 +9,9 @@ process.mode = 'preview';
 
 
 // TODO
-// The amphora secrion contains site specific details just to handle aps publications
-// It is currently a tactical solution to handle preview for aps publications which a
-// publication might consist of many resources need to be proxies
+// The amphora section contains site specific details just to handle aps publications
+// It is currently a tactical solution to handle preview for aps publications a
+// publication might consist of many resources need to be proxied including thumbnails
 // The proxy will be moved to nginx soon or a better way to handle resources will be investigated
 // (without being downloaded by preview server before serving) 
 if (config.amphora) {
