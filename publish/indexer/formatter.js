2a176f4faef0375db66cb9b8ba23c3f97b90cd9f
David Sinclair <david.sinclair@gov.scot>
2016-09-08 16:23:31 +0100

    MGS-1621 change the way search indexing is done

---
+++ publish/indexer/formatter.js
@@ -0,0 +1,81 @@
+
+'use strict';
+
+var path = require('path');
+var fs = require('fs');
+var fileOptions = {encoding: 'utf-8'};
+
+/**
+ * Formatter applied to all content items before they are indexed.
+ **/
+function format(item, srcdir, callback) {
+    // index the item using the search service
+    // we only want to index the content item (not andcestors etc.)
+    // but we need the url to be included
+
+    var formatted = JSON.parse(JSON.stringify(item.contentItem));
+
+    var fieldsToPromote = ['url', 'filterDate', 'label'];
+    fieldsToPomote.forEach(function (field) {
+        formatted[field] = item[field];
+    });
+    formatted._embedded.format.name = item.contentItem._embedded.format.name.toLowerCase();
+
+    formatTopics(formatted);
+
+    if (formatted._embedded.format.name === 'role' ||
+        formatted._embedded.format.name === 'featured_role') {
+
+        enrichRoleWithPersonData(item, formatted, srcdir, callback);
+    } else {
+        callback(formatted);
+    }
+ }
+
+function formatTopics(item) {
+    // create an array of topic names
+    item.topicNames = [];
+    if (item._embedded.topics) {
+        item._embedded.topics.forEach(function (t) {
+            item.topicNames.push(t.name);
+        });
+    }
+}
+
+function enrichRoleWithPersonData(item, formatted, srcdir, callback) {
+    if (item.relatedItems.hasIncumbent.length === 0) {
+        // no incumbent to enrich with
+        callback(formatted);
+        return;
+    }
+
+    var incumbentFilename = path.resolve(srcdir, item.relatedItems.hasIncumbent[0].uuid + '.json');
+
+
+    fs.readFile(incumbentFilename, fileOptions, function (err, data) {
+        if (err) {
+         callback(err);
+         return;
+        }
+
+        var incumbent = JSON.parse(data);
+
+        // copy all content fields into the role content item
+        ['content', 'additionalContent'].forEach(
+            function (field) {
+                formatted[field] = [item.contentItem[field], incumbent.contentItem[field]].join('\n');
+            });
+
+        // append the incumbent tags to the role tags
+        incumbent.contentItem.tags.forEach(function (tag) {
+            formatted.tags.push(tag);
+        });
+
+        callback(formatted);
+    });
+}
+
+
+module.exports = {
+    format : format
+};
---
+++ test/publish/indexer/formatter_spec.js
@@ -0,0 +1,186 @@
+var sutPath = '../../../out/instrument/publish/indexer/formatter';
+//var sutPath = '../../../publish/indexer/formatter';
+var sut = require(sutPath);
+
+describe('formatter', function() {
+
+    var tempDir;
+
+    beforeEach(function(done) {
+        var temp = require('temp');
+        temp.mkdir('',
+            function(err, dirPath) {
+                tempDir = dirPath;
+                done();
+            }
+        );
+    });
+
+    function item(formatname, topicNames, incumbent) {
+        var ret = {
+            url: 'url',
+            filterDate: 'filterDate',
+            contentItem: {
+                content: 'item content',
+                additionalContent: 'item additionalContent',
+                tags: ['tag1', 'tag2'],
+                _embedded: {
+                    format: {
+                        name: formatname
+                    }
+                }
+            },
+            relatedItems: {
+                hasIncumbent: []
+            }
+        };
+
+        if (topicNames) {
+            ret.contentItem._embedded.topics = [];
+
+            topicNames.forEach(function (topic) {
+                ret.contentItem._embedded.topics.push({ name: topic});
+            });
+        }
+
+        if (incumbent) {
+            ret.relatedItems.hasIncumbent.push({ uuid: incumbent});
+        }
+        return ret;
+    }
+
+    it('non role', function(done) {
+
+        // ARRANGE
+        var input = item('ARTICLE', ['topic1', 'topic2']);
+        var expected = JSON.parse(JSON.stringify(input)).contentItem;
+        expected.url = 'url';
+        expected.filterDate = 'filterDate';
+        expected.topicNames = ['topic1', 'topic2']
+        expected._embedded.format.name = 'article';
+
+        //ACT
+        sut.format(input, '/tmp', function (actual) {
+
+            // ASSERT
+            expect(actual).toEqual(expected);
+            done();
+        });
+
+
+    });
+
+    it('no topics', function(done) {
+
+        // ARRANGE
+        var input = item('ARTICLE', null);
+        var expected = JSON.parse(JSON.stringify(input)).contentItem;
+        expected.url = 'url';
+        expected.filterDate = 'filterDate';
+        expected.topicNames = []
+        expected._embedded.format.name = 'article';
+
+        //ACT
+        sut.format(input, '/tmp', function (actual) {
+
+            // ASSERT
+            expect(actual).toEqual(expected);
+            done();
+        });
+    });
+
+    it('role with no incumbent', function(done) {
+
+        // ARRANGE
+        var input = item('ROLE', null);
+        var expected = JSON.parse(JSON.stringify(input)).contentItem;
+        expected.url = 'url';
+        expected.filterDate = 'filterDate';
+        expected.topicNames = []
+        expected._embedded.format.name = 'role';
+
+        //ACT
+        sut.format(input, '/tmp', function (actual) {
+
+            // ASSERT
+            expect(actual).toEqual(expected);
+            done();
+        });
+    });
+
+    it('featured role with no incumbent', function(done) {
+
+        // ARRANGE
+        var input = item('FEATURED_ROLE', null);
+        var expected = input;
+
+        var expected = JSON.parse(JSON.stringify(input)).contentItem;
+        expected.url = 'url';
+        expected.filterDate = 'filterDate';
+        expected.topicNames = []
+        expected._embedded.format.name = 'featured_role';
+
+        //ACT
+        sut.format(input, '/tmp', function (actual) {
+
+            // ASSERT
+            expect(actual).toEqual(expected);
+            done();
+        });
+    });
+
+    it('featured role with non existant incumbent', function(done) {
+
+        // ARRANGE
+        var input = item('FEATURED_ROLE', null, 'does-not-exist');
+
+        //ACT
+        sut.format(input, '/tmp', function (actual) {
+
+            // ASSERT
+            expect(actual instanceof Error).toBe(true);
+            done();
+        });
+    });
+
+    function createTempFile(data) {
+        var itemPath = require('path').resolve(tempDir, data.uuid + '.json');
+        require('fs').writeFileSync(itemPath, JSON.stringify(data, null, '\t'));
+    }
+
+    it('featured role with incumbent', function(done) {
+
+        // ARRANGE
+        var input = item('ROLE', null, 'incumbent-uuid');
+
+        var incumbent = {
+            uuid: 'incumbent-uuid',
+            contentItem: {
+                content: 'incumbent content',
+                additionalContent : 'incumbent additionalContent',
+                tags: ['incumbent-tag1', 'incumbent-tag2']
+            }
+        }
+
+        // create a tmp directory and write out incumbent
+        createTempFile(incumbent);
+
+        var expected = JSON.parse(JSON.stringify(input)).contentItem;
+        expected.url = 'url';
+        expected.filterDate = 'filterDate';
+        expected.topicNames = []
+        expected._embedded.format.name = 'role';
+        expected.tags = ['tag1', 'tag2', 'incumbent-tag1', 'incumbent-tag2']
+        expected.content = expected.content + '\n' + incumbent.contentItem.content;
+        expected.additionalContent = expected.additionalContent + '\n' + incumbent.contentItem.additionalContent;
+
+        //ACT
+        sut.format(input, tempDir, function (actual) {
+
+            // ASSERT
+            expect(actual).toEqual(expected);
+            done();
+        });
+    });
+
+});
