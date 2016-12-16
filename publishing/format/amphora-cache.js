4117558d2648d89ced295f7136785ab0977e68c0
Evren Ozcan <evren.ozcan@scotland.gsi.gov.uk>
2016-12-16 09:33:11 +0000

    MGS-2156 Enhanced downloads to prevent redundant file copies

--- publishing/format/amphora-cache.js
+++ publishing/format/amphora-cache.js
@@ -6,6 +6,18 @@ var assembledCachePath = path.join(cachePath, 'assembled');
 var fs = require('fs-extra');
 fs.ensureDirSync(assembledCachePath);
 
+var getLocalPath = function(resource, callback) {
+    var cachedResource = path.join(cachePath, resource.storage.checksum);
+    fs.exists(cachedResource, function(exists) {
+        if (exists) {
+            console.log('Amphora', 'local-cache-path', resource.storage.checksum, '->', resource.destination);
+            callback(null, cachedResource);
+        } else {
+            callback(resource.storage.checksum + ' is NOT cached');
+        }
+    });
+};
+
 var getLocalResource = function(resource, callback) {
     var cachedResource = path.join(cachePath, resource.storage.checksum);
     fs.exists(cachedResource, function(exists) {
@@ -18,11 +30,15 @@ var getLocalResource = function(resource, callback) {
     });
 };
 
-var cacheResource = function(resource, callback) {
+var cacheResource = function(stream, resource, callback) {
     var cachedResource = path.join(cachePath, resource.storage.checksum);
     var file = fs.createWriteStream(cachedResource);
-    fs.createReadStream(resource.destination).pipe(file);
-    file.on('finish', callback)
+    stream.pipe(file);
+    file.on('finish', function() {
+        fs.ensureSymlink(cachedResource, resource.destination, function() {
+            callback();
+        });
+    });
 };
 
 var checkAssembledResource = function(uuid, callback) {
@@ -58,6 +74,7 @@ var storeAssembledResource = function(uuid, resource, callback) {
 };
 
 module.exports = {
+    getLocalPath: getLocalPath,
     getLocalResource: getLocalResource,
     cacheResource: cacheResource,
     checkAssembledResource: checkAssembledResource,
--- publishing/format/amphora-download-utils.js
+++ publishing/format/amphora-download-utils.js
@@ -18,34 +18,37 @@ var getContent = function(context, resource, callback) {
     });
 };
 
-var download = function(url, destination, callback) {
-    fs.ensureDir(path.dirname(destination), function() {
+var download = function(resourceItem, callback) {
+    var downloadRequest = request.get(resourceItem.url);
+    downloadRequest.on('response', function(response) {
+        if (response.statusCode === 200) {
+            console.log('Amphora', resourceItem.url);
+            cache.cacheResource(downloadRequest, resourceItem, callback);
+        } else {
+            callback({
+                error: 'Failed to download content',
+                status: response.statusCode,
+                url: resourceItem.url
+            });
+            downloadRequest.abort();
+        }
+    }).on('error', callback);
+};
 
-        var downloadRequest = request.get(url);
-        downloadRequest.on('response', function(response) {
-            if (response.statusCode === 200) {
-                console.log('Amphora', url);
-                var file = fs.createWriteStream(destination);
-                downloadRequest.pipe(file);
-                file.on('finish', callback);
-                file.on('error', function(e) {
-                    fs.unlink(destination, function(e) {
-                        callback({
-                            error: 'failed to stream content',
-                            details: e,
-                            destination: destination
-                        });
-                    });
-                });
-            } else {
-                callback({
-                    error: 'Failed to download content',
-                    status: response.statusCode,
-                    url: url
-                });
-                downloadRequest.abort();
-            }
-        }).on('error', callback);
+var downloadQueuedResource = function(app, resourceItem, callback) {
+    cache.getLocalPath(resourceItem, function(err, localPath) {
+        if (err) {
+            download(resourceItem, function(err) {
+                if (err) {
+                    app.context.errors.push(err);
+                }
+                callback(true);
+            });
+        } else {
+            fs.ensureSymlink(localPath, resourceItem.destination, function() {
+                callback(false);
+            });
+        }
     });
 };
 
@@ -54,26 +57,13 @@ var downloadQueuedResources = function(app, callback) {
     var fromCache = 0;
     var remote = 0;
     async.eachLimit(app.context.resourceQueue, 30, function(resourceItem, subCallback) {
-        cache.getLocalResource(resourceItem, function(err, stream) {
-            if (err) {
+        downloadQueuedResource(app, resourceItem, function(fromAmphora){
+            if (fromAmphora) {
                 remote++;
-                download(resourceItem.url, resourceItem.destination,
-                    function(err) {
-                        if (!err) {
-                            cache.cacheResource(resourceItem, subCallback);
-                        } else {
-                            app.context.errors.push(err);
-                            subCallback();
-                        }
-                    });
             } else {
                 fromCache++;
-                fs.ensureDir(path.dirname(resourceItem.destination), function() {
-                    var file = fs.createWriteStream(resourceItem.destination);
-                    stream.pipe(file);
-                    file.on('finish', subCallback);
-                });
             }
+            subCallback();
         });
     }, function(err) {
         if (app.context.resourceQueue.length > 0) {
@@ -91,22 +81,23 @@ var store = function(context, resource, callback) {
     var target = context.app.preview ? 'pdfs' : 'pages';
     var base = path.join('out', target, resource.metadata.namespace);
     var destination = path.join(base, filename);
+    var resourceItem = {
+        storage: {
+            checksum: resource.storage.checksum,
+        },
+        url: resource._links.inline.href,
+        destination: destination
+    };
     if (context.app.preview) {
         fs.exists(destination, function(exists) {
             if (exists) {
                 callback();
             } else {
-                download(resource._links.inline.href, destination, callback);
+                download(resourceItem, callback);
             }
         });
     } else {
-        context.app.context.resourceQueue.push({
-            storage: {
-                checksum: resource.storage.checksum,
-            },
-            url: resource._links.inline.href,
-            destination: destination
-        });
+        context.app.context.resourceQueue.push(resourceItem);
         callback();
     }
 };
@@ -114,7 +105,6 @@ var store = function(context, resource, callback) {
 module.exports = {
     store: store,
     getContent: getContent,
-    download: download,
     downloadQueuedResources: downloadQueuedResources,
     cache: cache
 };
