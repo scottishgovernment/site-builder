25a979996e668fddcb2d0a4601cd7a8900390a84
David Sinclair <david.sinclair@gov.scot>
2017-01-11 13:54:02 +0000

    MGS-2138 moved elastic search takss into a multiTask

---
+++ tasks/elasticsearch.js
@@ -0,0 +1,101 @@
+'use strict';
+
+/**
+ * Grunt task for indexing content with eleasticsearch.  There are two targets:
+ *
+ *   index-content:
+ *      this target indexes the content contained in out/contentitems to the
+ *      offline index.  It will also ensue that all of the required indices
+ *      and aliases are present.
+ *
+ *   flip-aliases:
+ *      this target switches the onlinecontent and offlinecontent aliases
+ **/
+module.exports = function(grunt) {
+    grunt.registerMultiTask('elasticsearch', 'Elastic search management', function() {
+        var config = require('config-weaver').config();
+        var site = require('../common/site')();
+        var done = this.async();
+
+        switch (this.target) {
+            case 'index-content' :
+                indexContent(grunt, config, site, done);
+            break;
+
+            case 'flip-aliases' :
+                flipAliases(grunt, config, site, done);
+            break;
+
+            default:
+                grunt.log.error('Unsupported target: ', this.target);
+                done(false);
+        }
+    });
+};
+
+function indexContent(grunt, config, site, done) {
+    var indexed = 0;
+    var skipped = 0;
+
+    function onStart(srcdir) {
+        grunt.log.writeln('Indexing ', srcdir['cyan']);
+    }
+
+    function onIndexed(url) {
+        grunt.log.writeln('Indexed ', url['cyan'], ' OK'['green']);
+        indexed++;
+    }
+
+    function onInfo(msg) {
+        grunt.log.writeln('Index content: ', msg['cyan']);
+        indexed++;
+    }
+
+    function onSkipped() {
+        skipped++;
+    }
+
+    function onDone(errs) {
+        if (errs) {
+            grunt.log.error('Error indexing content:', JSON.stringify(errs, null, '\t'));
+            done(false);
+        } else {
+            grunt.log.writeln('Index finshed'['green'], indexed, 'indexed, ', skipped, 'skipped');
+            done(true);
+        }
+        return;
+    }
+
+    var indexer = require('../publish/indexer/indexer.js').create(config, site);
+    indexer
+        .on('start',  onStart)
+        .on('indexed', onIndexed)
+        .on('info', onInfo)
+        .on('skipped', onSkipped)
+        .on('done',  onDone)
+        .index(grunt.config('site.contentitems'));
+}
+
+function flipAliases(grunt, config, site, done) {
+    grunt.log.writeln('Swap search index aliases ');
+
+    var es = require('elasticsearch');
+    var esClient = new es.Client(config.elasticsearch);
+    var configuratorClass = require('../publish/indexer/index-configurator');
+    var listener = {
+            info : function (msg) {
+                grunt.log.writeln(msg);
+            }
+        };
+    var indexConfigurator = new configuratorClass(config, site, listener, esClient);
+
+    indexConfigurator.swapAliasTargets(function (err) {
+        esClient.close();
+        if (err) {
+            grunt.log.error(grunt.log.writeln('Flip aliases ', err['red']));
+            done(false);
+        } else {
+            done(true);
+        }
+    });
+}
--- tasks/swap-search-index-aliases.js
+++ tasks/swap-search-index-aliases.js
@@ -2,6 +2,8 @@
 
 // Grunt task that switches the livecontent / offlinecontent aliases.  Called oncea new site has been copied
 // to nginx.
+var es = require('elasticsearch');
+
 module.exports = function(grunt) {
 
     grunt.registerTask('swap-search-index-aliases', 'index content from site.contentitems',
@@ -11,16 +13,18 @@ module.exports = function(grunt) {
 
             var config = require('config-weaver').config(),
                 site = require('../common/site')(),
+                esClient = new es.Client(config.elasticsearch),
                 configuratorClass = require('../publish/indexer/index-configurator'),
                 listener = {
                     info : function (msg) {
                         grunt.log.writeln('Swap search index aliases ', msg['cyan']);
                     }
                 },
-                indexConfigurator =  new configuratorClass(config, site, listener, this.esClient),
+                indexConfigurator =  new configuratorClass(config, site, listener, esClient),
                 done = this.async();
 
             indexConfigurator.swapAliasTargets(function (err) {
+                esClient.close();
                 if (err) {
                     grunt.log.error(grunt.log.writeln('Swap search index aliases ', err['red']));
                     done(false);
