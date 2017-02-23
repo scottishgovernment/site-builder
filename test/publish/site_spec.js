3ee0bff523e2428ae66ad7993e3bf23a319bc89c
Evren Ozcan <evren.ozcan@scotland.gsi.gov.uk>
2017-02-23 12:05:09 +0000

    MGS-2482 updated site tests to use json

--- test/publish/site_spec.js
+++ test/publish/site_spec.js
@@ -18,12 +18,12 @@ describe('the site build', function() {
         var items = path.join(resources, 'items');
         var out = path.join(process.cwd(), 'out/pages');
         var site = require(sutPath);
-        var siteBuilder = new site.Site(items, out, renderer);
+        var siteBuilder = new site.Site(items, renderer);
         siteBuilder.build(function(err) {
             if (err) {
                 return done('build failed: ' + err);
             }
-            fs.stat('out/pages/01/index.html', function (err, data) {
+            fs.stat(path.join(items, 'pages/01/index.html'), function (err, data) {
                 if (err) {
                     return done('output file not generated: ' + err);
                 }
@@ -33,7 +33,8 @@ describe('the site build', function() {
     });
 
     it('rewrites internal links', function(done) {
-        fs.readFile('out/pages/02/index.html', function (err, data) {
+        var items = path.join(resources, 'items');
+        fs.readFile(path.join(items, 'pages/02/index.html'), function (err, data) {
             if (err) {
                 return done(err);
             }
@@ -43,3 +44,4 @@ describe('the site build', function() {
     });
 
 });
+
