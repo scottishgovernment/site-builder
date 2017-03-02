49e8f0a4b8f89f7bd5799497f79d1a60ccf73782
Evren Ozcan <evren.ozcan@scotland.gsi.gov.uk>
2017-03-02 11:16:45 +0000

    MGS-2497 updated site tests to reflect real life cases

--- test/publish/site_spec.js
+++ test/publish/site_spec.js
@@ -4,19 +4,27 @@ var sutPath = '../../out/instrument/publish/site';
 describe('the site build', function() {
 
     var path = require('path');
-    var fs = require('fs');
+    var fs = require('fs-extra');
     var resources = path.join(process.cwd(), 'test/render/site');
+    var out = path.join(process.cwd(), 'out');
 
     function sampleCachableItems() {
         return [
-            { id: 'SITE-01', hash: '1hash', uuid: '/01/' },
-            { id: 'SITE-02', hash: '2hash', uuid: '/02/' },
-            { id: 'structural', hash: 'structural', uuid: '/structural/' },
+            { id: 'SITE-01', hash: '1hash', url: '/01/' },
+            { id: 'SITE-02', hash: '2hash', url: '/02/' },
+            { id: 'structural', hash: 'structural', url: '/structural/' },
         ];
     }
+    try {
+        fs.unlinkSync(path.join(out, 'pages', '01', 'index.html'));
+        fs.unlinkSync(path.join(out, 'pages', '02', 'index.html'));
+    } catch (err) {
 
-    fs.writeFileSync(path.join(resources, 'items', '/siteIndex.json'), JSON.stringify(sampleCachableItems()));
+    }
 
+    fs.copySync(path.join(resources, 'items'), out);
+
+    fs.writeFileSync(path.join(out, 'siteIndex.json'), JSON.stringify(sampleCachableItems()));
 
     it('generates pages', function(done) {
         var render = require(renderPath);
@@ -24,16 +32,13 @@ describe('the site build', function() {
         var partials = path.join(resources, 'partials');
         var helpers = path.join(resources, 'helpers');
         var renderer = new render.Renderer(layouts, partials, helpers);
-
-        var items = path.join(resources, 'items');
-        var out = path.join(process.cwd(), 'out/pages');
         var site = require(sutPath);
-        var siteBuilder = new site.Site(items, renderer);
+        var siteBuilder = new site.Site(out, renderer);
         siteBuilder.build(function(err) {
             if (err) {
                 return done('build failed: ' + err);
             }
-            fs.stat(path.join(items, 'pages/01/index.html'), function(err, data) {
+            fs.stat(path.join(out, 'pages/01/index.html'), function(err, data) {
                 if (err) {
                     return done('output file not generated: ' + err);
                 }
@@ -43,8 +48,7 @@ describe('the site build', function() {
     });
 
     it('rewrites internal links', function(done) {
-        var items = path.join(resources, 'items');
-        fs.readFile(path.join(items, 'pages/02/index.html'), function(err, data) {
+        fs.readFile(path.join(out, 'pages/02/index.html'), function(err, data) {
             if (err) {
                 return done(err);
             }
