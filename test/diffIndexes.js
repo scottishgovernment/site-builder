f1f34663234f193af303ba6c10ef8c9ff9520466
David Rubio <david.vidal@gov.scot>
2017-04-05 16:28:16 +0100

    Updated diffindexes to filter uncacheable items

--- test/diffIndexes.js
+++ test/diffIndexes.js
@@ -1,9 +1,13 @@
 const siteIndexes = require('../publish/site-indexes')();
 const fs = require('fs');
 
-const leftIndex = JSON.parse(fs.readFileSync(process.argv[2]));
-const rightIndex = JSON.parse(fs.readFileSync(process.argv[3]));
+var leftIndex = JSON.parse(fs.readFileSync(process.argv[2]));
+var rightIndex = JSON.parse(fs.readFileSync(process.argv[3]));
 
-const diff = siteIndexes.diffBuildIndexes(leftIndex, rightIndex);
+// filter out uncacheable items
+var leftIndexCacheable = leftIndex.filter(i => i.cacheable);
+var rightIndexCacheable = rightIndex.filter(i => i.cacheable);
+
+const diff = siteIndexes.diffBuildIndexes(leftIndexCacheable, rightIndexCacheable);
 console.log('Changed or New: ', JSON.stringify(diff.changedOrNew, null, '\t'));
 console.log('Deleted       : ', JSON.stringify(diff.deleted, null, '\t'));
