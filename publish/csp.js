13064f214fbabdf6168151fa5be118636f9e6dd5
Gary Carmichael <gary.carmichael@gov.scot>
2017-05-11 11:39:57 +0100

    MGS-2721 Add Content-Security-Policy header creation.
    Adds a grunt task that assembles a Content-Security-Policy
    HTTP header, pulled from a template whitelist in the
    resources directory of a specific site.
    
    The header created is in Report-Only mode until further
    tests have been conducted.

---
+++ publish/csp.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var fs = require('fs-extra');
+var path = require('path');
+
+function Csp(tempDir, jsonPath) {
+    this.tempDir = tempDir;
+    this.cspDir = path.join(this.tempDir, 'nginx');
+    this.cspFile = path.join(this.cspDir, 'csp.conf');
+
+    // Can optionally accept a different template path
+    this.jsonPath = jsonPath || 'resources/csp.json';
+}
+
+/**
+ * Assembles then writes a CSP header to an nginx
+ * configuration file for inclusion in site headers.
+ */
+Csp.prototype.writeHeader = function(done) {
+    if (!fs.existsSync(this.jsonPath)) {
+        console.log('No CSP template found. CSP header not written.');
+        return done();
+    }
+
+    console.log('Building CSP header.');
+
+    var json = fs.readFileSync(this.jsonPath, 'UTF-8');
+    var template = JSON.parse(json);
+
+    // Add any additional CSP directives required from template here.
+    var directives = [
+      'script-src',
+      'img-src',
+      'style-src',
+      'font-src',
+      'frame-src',
+      'object-src',
+      'report-uri'
+    ];
+
+    var clauses = [];
+    for (var i = 0; i < directives.length; i++) {
+        var directive = directives[i];
+        if (template[directive]) {
+            clauses.push(directive + ' ' + template[directive].join(' '));
+        }
+    }
+    var header = '';
+    if (clauses.length) {
+        header = 'add_header Content-Security-Policy-Report-Only "' +
+            clauses.join('; ') +
+            '";';
+    }
+
+    fs.mkdirsSync(this.cspDir);
+    fs.writeFileSync(this.cspFile, header, 'UTF-8');
+
+    done();
+};
+
+function create(tempDir, jsonPath) {
+    return new Csp(tempDir, jsonPath);
+}
+
+module.exports = {
+    create: create
+};
