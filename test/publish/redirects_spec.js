5dd3ee3f5c0e3af1512adf8219fb5f6dd813f799
Martin Ellis <martin.ellis@gov.scot>
2017-07-27 12:29:47 +0100

    MGS-3418 Use exact matching on the alias in url_aliases.csv

--- test/publish/redirects_spec.js
+++ test/publish/redirects_spec.js
@@ -7,21 +7,29 @@ describe('redirects', function() {
 
         // ARRANGE
         var redirects = [{
-            url: '/url1/',
-            alias: '/url1/alias1'
+            alias: '/url1/alias1',
+            url: '/url1/'
         }, {
-            url: '/url1/',
-            alias: '/url1/alias2'
+            alias: '/url1/alias2',
+            url: '/url1/'
         }, {
-            url: '/url2/with/path/',
-            alias: '/url2'
+            alias: '/url2',
+            url: '/url2/with/path/'
+        }, {
+            alias: '/url3',
+            url: '/no-trailing-slash'
+        }, {
+            alias: '/url4',
+            url: 'https://external.url/index.html'
         }];
 
         var sut = require(sutPath)('/tmp/');
 
-        var expectedContent = 'rewrite ^/url1/alias1(/?|/.*)$ /url1$1 permanent ;\n' +
-            'rewrite ^/url1/alias2(/?|/.*)$ /url1$1 permanent ;\n' +
-            'rewrite ^/url2(/?|/.*)$ /url2/with/path$1 permanent ;\n';
+        var expectedContent = 'rewrite ^/url1/alias1/?$ /url1/ permanent ;\n' +
+            'rewrite ^/url1/alias2/?$ /url1/ permanent ;\n' +
+            'rewrite ^/url2/?$ /url2/with/path/ permanent ;\n' +
+            'rewrite ^/url3/?$ /no-trailing-slash permanent ;\n' +
+            'rewrite ^/url4/?$ https://external.url/index.html permanent ;\n';
 
         sut.create(redirects, function() {
             // ASSERT - the temp directory should contain the expected files
